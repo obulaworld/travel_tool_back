f6103c24db7f88e67f8063ad72a052bc1ae08df0
JohnM <johnmutuma5@gmail.com>
2018-10-05 10:26:22 +0300

    adjust seed image url (#91)

--- src/database/seeders/20181004161227-demo-guest-house.js
+++ src/database/seeders/20181004161227-demo-guest-house.js
@@ -6,7 +6,7 @@ module.exports = {
         houseName: 'Mini flat-B',
         location: 'Lagos,Nigeria',
         bathRooms: '1',
-        imageUrl: 'https://www.lol.com',
+        imageUrl: 'https://www.dropbox.com/s/c2n63x2jamh3ndv/guesthouse2.jpg?raw=1',
         createdAt: '2018-08-16 012:11:52.181+01',
         updatedAt: '2018-08-16 012:11:52.181+01',
         userId: '-LJV4b1QTCYewOtk5F63'
@@ -16,7 +16,7 @@ module.exports = {
         houseName: 'Major flat-A',
         location: 'Nairobi,Kenya',
         bathRooms: '1',
-        imageUrl: 'https://www.lol.com',
+        imageUrl: 'https://www.dropbox.com/s/jpv2axwo29qzl18/guesthouse1.jpg?raw=1',
         createdAt: '2018-08-16 012:11:52.181+01',
         updatedAt: '2018-08-16 012:11:52.181+01',
         userId: '-LJV4b1QTCYewOtk5F63'
