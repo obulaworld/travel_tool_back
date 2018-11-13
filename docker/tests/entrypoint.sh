c561c9928fdece20749f360316894df307bbe3de
Dave Mathews Njeru <dave.njeru@andela.com>
2018-11-13 14:13:18 +0300

    fix(yarn.lock): fix yarn version inconsistencies (#168)
    - add yarn@^1.10.1 in circleci install command
    - add yarn@^1.10.1 in the respective Dockerfiles
    
    [Delivers #161775747]

--- docker/tests/entrypoint.sh
+++ docker/tests/entrypoint.sh
@@ -8,6 +8,7 @@ yarn lint
 printf "\n\n======================================\n"
 printf "Running tests"
 printf "\n======================================\n\n"
+export NODE_ENV=test
 yarn test
 
 exit 0
