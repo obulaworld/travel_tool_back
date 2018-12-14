fb7f56b28119ee713eec29155939fb40835ae413
Celestine Ekoh-Ordan <CEO.ehis@outlook.com>
2018-12-14 10:46:49 +0100

    ch(e2e) update users seeder for e2e tests
    - add new user to seeder
    
    [Finishes #162375845]

--- src/database/seeders/e2e/20180927152303-create-user.js
+++ src/database/seeders/e2e/20180927152303-create-user.js
@@ -18,7 +18,22 @@ module.exports = {
         createdAt: '2018-08-15 012:11:53.181+01',
         updatedAt: '2018-08-15 012:11:53.181+01',
         picture: 'https://lh5.googleusercontent.com/-PbuF53uxx4U/AAAAAAAAAAI/AAAAAAAAAAA/AGDgw-i0-XeoeYYk7TpfNBvulhV0oFM6eg/mo/photo.jpg?sz=50'
-      }
+      },
+      {
+        id: 2,
+        fullName: 'John Doe',
+        email: 'john.doe@andela.com',
+        userId: '-LMgZQKq6MXAj_41iRWi',
+        passportName: 'John Doe',
+        department: 'Talent & Development',
+        occupation: 'Software developer',
+        manager: 'Travela Test',
+        gender: 'Male',
+        location: 'Lagos',
+        createdAt: '2018-08-15 012:11:53.181+01',
+        updatedAt: '2018-08-15 012:11:53.181+01',
+        picture: 'https://lh5.googleusercontent.com/-PbuF53uxx4U/AAAAAAAAAAI/AAAAAAAAAAA/AGDgw-i0-XeoeYYk7TpfNBvulhV0oFM6eg/mo/photo.jpg?sz=50'
+      },
     ],
     {},
   ),
